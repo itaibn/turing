d0424f140907d3c8f3f18023dce1283466ffc659
Itai Bar-Natan <itaibn3141@gmail.com>
2017-05-09 17:52:42 -0400

    Automatically change viewpoint to see tape head

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,37 @@ use std::rc::Rc;
 
 use turing::{Tape, TuringMachineComputation, Symbol};
 
+const VISIBLE_CELLS: i32 = 21;
+
+struct GuiState {
+    run: TuringMachineComputation,
+    view_start: i32,
+}
+
+impl GuiState {
+    fn step(&mut self) -> bool{
+        if self.run.step() {return true;}
+        if self.run.tape_head_position() < self.view_start {
+            self.view_start -= 5;
+        }
+        if self.run.tape_head_position() >= self.view_start + VISIBLE_CELLS {
+            self.view_start += 5;
+        }
+        //DEBUG
+        //println!("Debug:\n tape head position: {}\n View start point: {}",
+        //    self.run.tape_head_position(), self.view_start);
+        false
+    }
+}
+
 fn main() {
     let mut rng = rand::StdRng::new().unwrap();
     let turing_machine = Rc::new(turing::random_turing_machine(&mut rng, 30));
-    let computation = Rc::new(RefCell::new(
-        TuringMachineComputation::start(turing_machine)));
+    let gui_state_owned = GuiState {
+        run: TuringMachineComputation::start(turing_machine),
+        view_start: -10,
+    };
+    let gui_state = Rc::new(RefCell::new(gui_state_owned));
 
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
@@ -33,14 +59,16 @@ fn main() {
     });
 
     let tm_view: gtk::DrawingArea = builder.get_object("tm-view").unwrap();
-    let computation_clone = computation.clone();
-    tm_view.connect_draw(move |_, ctx| draw_tape(ctx,
-        &*computation_clone.borrow()));
+    let gui_state_clone = gui_state.clone();
+    tm_view.connect_draw(move |_, ctx| {
+        draw_tape(ctx, &*gui_state_clone.borrow());
+        Inhibit(false)
+    });
 
-    let computation_clone = computation.clone();
+    let gui_state_clone = gui_state.clone();
     let tm_view_clone = tm_view.clone();
     gtk::timeout_add(1000, move || {
-        let halted = computation_clone.borrow_mut().step();
+        let halted = gui_state_clone.borrow_mut().step();
         tm_view_clone.queue_draw();
         if halted {println!("Halted")}
         Continue(!halted)
@@ -49,14 +77,15 @@ fn main() {
     gtk::main();
 }
 
-fn draw_tape(cr: &cairo::Context, computation: &TuringMachineComputation) ->
-    Inhibit {
+fn draw_tape(cr: &cairo::Context, gui_state: &GuiState) {
 
     const CELL_HEIGHT: f64 = 30.0;
     const CELL_WIDTH: f64 = 30.0;
 
+    let ref computation = gui_state.run;
     let tape = computation.tape();
-    for n in -10..11 {
+    for j in 0..VISIBLE_CELLS {
+        let n = gui_state.view_start + j;
         match tape.read_at(n) {
             Symbol::Zero => {
                 cr.set_source_rgb(1.0, 1.0, 1.0);
@@ -65,15 +94,13 @@ fn draw_tape(cr: &cairo::Context, computation: &TuringMachineComputation) ->
                 cr.set_source_rgb(0.0, 0.0, 0.0);
             }
         }
-        cr.rectangle(((n+10) as f64)*CELL_WIDTH, 0.0, CELL_WIDTH, CELL_HEIGHT);
+        cr.rectangle((j as f64)*CELL_WIDTH, 0.0, CELL_WIDTH, CELL_HEIGHT);
         cr.fill();
     }
 
     cr.set_source_rgb(1.0, 0.0, 0.0);
-    cr.move_to((computation.tape_head_position() + 10) as f64 * CELL_WIDTH,
-        CELL_HEIGHT/2.0);
+    cr.move_to((computation.tape_head_position() - gui_state.view_start) as f64
+        * CELL_WIDTH, CELL_HEIGHT/2.0);
     //cr.move_to(15.0, 10.0);
     cr.show_text(&computation.current_state().to_string());
-
-    Inhibit(false)
 }
